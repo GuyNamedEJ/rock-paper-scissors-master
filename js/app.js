dcf996d24f9012ec21bd77d2bf78a48e85c75f8f
EJ Milord <jmilord7@gmail.com>
2022-10-09 17:18:22 -0400

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,113 @@
+// Game Variables
+let playerPick;
+let aiPick;
+let options = ["rock", "paper", "scissors"];
+let score = localStorage.getItem("score");
+
+// Get references to the buttons
+let rock = document.getElementById("rock");
+let paper = document.getElementById("paper");
+let scissors = document.getElementById("scissors");
+
+// Get references to variables
+let pickPanel = document.getElementById("stage-1");
+let aiSelect = document.getElementById("stage-2");
+let resultPanel = document.getElementById("result-panel");
+let resultText = document.getElementById("result-text");
+let playerPickDisplay = document.getElementById("player-pick-container");
+let scoreDisplay = document.getElementById("score-display");
+let aiPickDisplay = document.getElementById("ai-pick-container");
+let playerPickIcon = document.getElementById("player-pick");
+let aiPickIcon = document.getElementById("ai-pick");
+let resetBtn = document.getElementById('reset-btn')
+
+rock.addEventListener("click", GetPlayerPick)
+paper.addEventListener("click", GetPlayerPick)
+scissors.addEventListener("click", GetPlayerPick)
+resetBtn.addEventListener("click", PlayAgain)
+scoreDisplay.textContent = localStorage.getItem("score")
+
+/**
+ * Game is broken up into 3 stages
+ * Stage 1 - Player selects
+ *  - When the player picks, hold reference to selection
+ *  - hide select panel and show compare panel
+ *  - set player pick icon to the player's selection
+ * Stage 2 - AI selects
+ * - after a delay, player makes random selection
+ * - display the computer selection
+ * Stage 3 - determine winner
+ * -
+ *  */
+
+function GetPlayerPick() {
+  playerPick = this.id;
+  pickPanel.style.display = "none";
+  aiSelect.style.display = "flex";
+  DisplayPicks();
+}
+
+function GetAIPick() {
+  let random = Math.floor(Math.random() * 3);
+  aiPick = options[random];
+  aiPickDisplay.classList.add(`${aiPick}`);
+  aiPickIcon.src = `/images/icon-${aiPick}.svg`;
+  
+}
+
+function ComparePicks() {
+  // Rock Beats Scissors
+  // Scissors beats Paper
+  // Paper beats Rock
+  if((playerPick === 'rock' && aiPick === 'scissors') ||
+  (playerPick === 'scissors' && aiPick === 'paper') ||
+  (playerPick === 'paper' && aiPick === 'rock'))
+  {
+    Win()
+  }
+  // Draw
+  else if (playerPick === aiPick) 
+  {
+    Draw()
+  }
+
+  else{
+    Lose()
+  }
+
+  resultPanel.style.display = "flex";
+}
+
+function Win() {
+  score++;
+  localStorage.setItem("score", score);
+  resultText.textContent = "You Win";
+  scoreDisplay.textContent = localStorage.getItem("score");
+}
+
+function Lose() {
+  resultText.textContent = "You Lose";
+}
+
+function Draw() {
+    resultText.textContent = "Draw";
+  }
+
+function DisplayPicks() {
+  playerPickDisplay.classList.add(`${playerPick}`);
+  playerPickIcon.src = `/images/icon-${playerPick}.svg`;
+  //setTimeout(GetAIPick,500)
+  GetAIPick();
+  ComparePicks();
+}
+
+
+function PlayAgain()
+{
+    resultPanel.style.display = 'none'
+    aiSelect.style.display = 'none'
+    pickPanel.style.display = 'block'
+    playerPickDisplay.classList.remove(`${playerPick}`);
+    aiPickDisplay.classList.remove(`${aiPick}`);
+    aiPickIcon.src = " ";
+}
